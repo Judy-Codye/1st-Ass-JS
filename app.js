56fce00c812a1fede59252a67410192ef2a63309
Nasira Siddiqui <ns670825@gmail.com>
2025-07-03 01:15:36 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,102 @@
+// // Chapter-1 Alert
+
+// Q-1
+// alert("Hello World");
+
+// Q-2
+// alert("Error!Please enter a valid password");
+
+// Q-3
+// alert("Welcome to js land....\n Happy Coding!");
+
+// Q-4
+// alert("Welcome to JS land...");
+// alert("Happy Coding! prevent this page from creating additional dialogs.");
+
+// Q-5
+// console.log("Hello...I can run JS through my web brower's console");
+// Q-6
+// alert("Congtatulation!");
+
+// Q-7
+//  Practice placement of <script></script> element in
+// following sections of your project in exercise 6:
+
+// a. Head
+
+// Before the <body>.
+//  <head>
+//     <script src="script.js"></script>
+// </head> 
+
+// b. Body (before your page’s HTML)
+
+// Place right after the opening <body> tag.
+//  <body>
+//     <script src="script.js"></script>
+// </body> 
+
+// c. Body (inside your page’s HTML)
+
+// // Placing the  <script> element with in the  <body> content.
+// <h1>hello world</h1>
+// <script src="script.js"></script>
+// <h1>hello world</h1>
+
+// d. Body (after your page’s HTML)
+
+// placing the  <script> element just before the closing </body> tag
+//  <h1>hello world</h1>
+// <script src="script.js"></script> 
+
+
+// // Chapter-2 variable
+
+// Q-1
+// var userName = "Nasira";
+// alert(userName);
+
+// Q-2
+// var myFullname = "Nasira Siddiqui";
+// alert(myFullname);
+
+// Q-3
+// var message = "Hello World";
+// alert(message);
+
+// Q-4
+// var studentName = "Jhone Doe";
+// alert(studentName);
+
+// var age = "15 years old";
+// alert(age);
+
+// var course = "Certified Mobile Application Development";
+// alert(course);
+
+// Q-5
+// var name = "PIZZA"
+// alert("PIZZA\nPIZZ\nPIZ\nPI\np");
+
+
+// Q-6
+// var email = ("My email address is example@example.com");
+// alert(email);
+
+// // Q-7
+// var book = ("I am trying to learn from the Book A smarter way to learn JavaScript");
+// alert(book);
+
+// // Q-8
+// var message = ("Yah! I can write HTML content through JavaScript");
+
+// document.write("Yah! I can write HTML content through JavaScript");
+
+// console.log(message);
+
+// // Q-9
+// var stringLine = ("▬▬▬▬▬▬▬▬▬௹⨝ῼ⨝௹▬▬▬▬▬▬▬▬▬")
+// alert(stringLine);
+// console.log(stringLine);
+
+
